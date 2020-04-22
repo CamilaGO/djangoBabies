54b5847ff13502bcda99831daabb1e000001345f
CamilaGO <gon18398@uvg.edu.gt>
2020-04-22 11:03:12 -0500

    Views listas

--- example/babies/views.py
+++ example/babies/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from guardian.shortcuts import assign_perm
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from permissions.services import APIPermissionClassFactory
+from babies.models import Baby
+from babies.serializer import BabySerializer
+from events.serializer import EventSerializer
+
+def evaluate(user, obj, request):
+    return user.first_name == obj.parent.name
+
+class BabyViewSet(viewsets.ModelViewSet):
+    queryset = Baby.objects.all()
+    serializer_class = BabySerializer
+    permission_classes = (
+        APIPermissionClassFactory(
+            name='EventPermission',
+            permission_configuration={
+                'base': {
+                    'create': True,
+                    'list': True,
+                },
+                'instance': {
+                    'retrieve': 'babies.view_baby',
+                    'destroy': False,
+                    'update': True,
+                    'partial_update': 'babies.change_baby',
+                }
+            }
+        ),
+    )
+
+    def perform_create(self, serializer):
+        baby = serializer.save()
+        user = self.request.user
+        assign_perm('babies.view_baby', user, baby)
+        assign_perm('babies.change_baby', user, baby)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['get'])
+    def events(self, request, pk=None):
+        baby = self.get_object()
+        return (Events.objects.filter(baby=baby).data)
--- example/events/views.py
+++ example/events/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from guardian.shortcuts import assign_perm
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from permissions.services import APIPermissionClassFactory
+from events.models import Event
+from events.serializer import EventSerializer
+
+def evaluate(user, obj, request):
+    return user.first_name == obj.baby.parent.name
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = (
+        APIPermissionClassFactory(
+            name='EventPermission',
+            permission_configuration={
+                'base': {
+                    'create': True,
+                    'list': True,
+                },
+                'instance': {
+                    'retrieve': 'events.view_event',
+                    'destroy': evaluate,
+                    'update': evaluate,
+                    'partial_update': 'events.change_event',
+                }
+            }
+        ),
+    )
+
+    def perform_create(self, serializer):
+        event = serializer.save()
+        user = self.request.user
+        assign_perm('events.change_event', user, event)
+        assign_perm('events.view_event', user, event)
+        return Response(serializer.data)
